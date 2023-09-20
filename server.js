3fe96b322bb8874229b5e444f4d4f0daee1ee486
Bhargavijk <Pothavaram.Bhargavi@jktech.com>
2023-09-20 14:38:20 +0530

    changes

--- server.js
+++ server.js
@@ -15,40 +15,140 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 app.post('/addEmpTimeSheet', async (req, res) => {
-    const{ name,daterange,timesheetsRows,totalhours} =req.body;
- 
- console.log('name',name,'daterange',daterange,'timesheetsRows',timesheetsRows,'totalhours',totalhours)
- 
- const job = await prisma.employee.create({
-    data:{
-     name:req.body.name,
-     daterange:req.body.daterange,
-     totalhours:req.body.totalhours,
-     timesheetsRows:{
-         createMany:{ 
-          data:req.body.timesheetsRows
-         }
-         
-     }
-    }
-  });
-  console.log(job);
+  const{ name,daterange,timesheetsRows,totalhours} =req.body;
+  const finalTimeSheetRows =timesheetsRows.map(ele=>{
+    ele.daterange=daterange;
+    return ele;
+});
+console.log('finalTimeSheetRows:',finalTimeSheetRows)
+console.log('name',name,'daterange',daterange,'timesheetsRows',timesheetsRows,'totalhours',totalhours)
+const recordExist=await prisma.employee.findFirst({
+where:{
+  name:req.body.name,
+  daterange:req.body.daterange
+},
+
+})
+if(recordExist && recordExist?.name){
+console.log('exist:',recordExist);
+res.json({status:200,message:'record exist'})
+}else{
+console.log('not exist:');
+const job = await prisma.employee.create({
+  data:{
+   name:req.body.name,
+   daterange:req.body.daterange,
+   totalhours:req.body.totalhours,
+   timesheetsRows:{
+       createMany:{ 
+        data:finalTimeSheetRows
+       }
+       
+   }
+  }
+});
+console.log(job);
 //  res.json(job);
- res.json({status:200,message:'created successfully'})
+res.json({status:200,message:'created successfully'})
+}
+
+//  return true;
+   
+});
+
+// app.post('/addEmpTimeSheet', async (req, res) => {
+//     const{ name,daterange,timesheetsRows,totalhours} =req.body;
+//     const preparedTimesheetRows =timesheetsRows.map(ele=>{
+//       ele.daterange=daterange
+//       return ele;
+//   });
+//   console.log('prepared',preparedTimesheetRows)
+//  console.log('name',name,'daterange',daterange,'timesheetsRows',timesheetsRows,'totalhours',totalhours)
+//  const recordExist=await prisma.employee.findFirst({
+//   where:{
+//     name:req.body.name,
+//     daterange:daterange
+//   },
+ 
+// })
+// if(recordExist && recordExist?.name){
+//   console.log('exist:',recordExist);
+//   res.json({status:401,message:'Already exist'})
+// }else{
+//   const job = await prisma.employee.create({
+//         data:{
+//          name:req.body.name,
+//          daterange:req.body.daterange,
+//          totalhours:req.body.totalhours,
+//          timesheetsRows:{
+//              createMany:{ 
+//               data:preparedTimesheetRows
+//              }
+             
+//          }
+//         }
+//       });
+//       console.log(job);
+//   console.log('not exist:');
+//   res.json({status:200,message:'created successfully'})
+// }
+
+//  const job = await prisma.employee.create({
+//     data:{
+//      name:req.body.name,
+//      daterange:req.body.daterange,
+//      totalhours:req.body.totalhours,
+//      timesheetsRows:{
+//          createMany:{ 
+//           data:req.body.timesheetsRows
+//          }
+         
+//      }
+//     }
+//   });
+//   console.log(job);
+//  res.json(job);
+ 
 //  return true;
      
- });
+
 
  app.get("/getdata", async (req, res) => {
   try {
-    const getdetails = await prisma.employee.findMany({
-      // where:{status:{
-      //   notIn:['approve','rejected']
-      // }},
-      include : {
-        timesheetsRows : true
-      }
-    });
+    let getdetails;
+    if(req.query.daterange){
+      getdetails=await prisma.employee.findMany({
+        where:{
+          daterange:req.query.daterange
+      },
+        include : {
+          timesheetsRows : true,
+          // where:{
+          //   projectCode:"WFS_1101"
+          // }
+        }
+      });
+    }else{
+      getdetails = await prisma.employee.findMany({
+        // where:{status:{
+        //   notIn:['approve','rejected']
+        // }},
+        include : {
+          timesheetsRows : true,
+          // where:{
+          //   projectCode:"WFS_1101"
+          // }
+        }
+      });
+    }
+    // const getdetails = await prisma.employee.findMany({
+    //   // where:{status:{
+    //   //   notIn:['approve','rejected']
+    //   // }},
+    //   include : {
+    //     timesheetsRows : true
+    //   }
+    // });
     let mainArr=[];
       for (let index = 0; index < getdetails.length; index++) {
         const element = getdetails[index];
@@ -98,12 +198,15 @@ app.get('/getEmpDetails',async(req,res)=>{
         .json({ success: false, message: "Error listing employeedata" });
   }
 });
+
+
+
 app.put("/timesheetActivity", async (req, res) => {
   try{
     console.log('data',req?.body);
-    const Resp = await prisma.timesheet.update(
+    const Resp = await prisma.timesheet.updateMany(
       {
-        where: { id: Number(req?.body?.id) },
+        where: { id: {in :req?.body?.id} },
         data: { "status": req?.body.status }
       })
     console.log('resp:',Resp);
@@ -119,6 +222,27 @@ app.put("/timesheetActivity", async (req, res) => {
   }
  
 });
+// app.put("/timesheetActivity", async (req, res) => {
+//   try{
+//     console.log('data',req?.body);
+//     const Resp = await prisma.timesheet.update(
+//       {
+//         where: { id: Number(req?.body?.id) },
+//         data: { "status": req?.body.status }
+//       })
+//     console.log('resp:',Resp);
+//     res.status(200).json(Resp);
+    
+//   }
+//   catch (error) {
+//     console.log('error',error);
+//     res
+//       .status(500)
+//       .json({ success: false, message: "Error listing employeedata" });
+//     console.error("Error creating user:", error);
+//   }
+ 
+// });
 
 
 // app.post("/addEmpTimeSheet", async (req, res) => {
