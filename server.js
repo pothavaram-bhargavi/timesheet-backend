bfe613c54492222deeca974bedb22bfb6a6f583d
Bhargavijk <Pothavaram.Bhargavi@jktech.com>
2023-09-25 10:59:57 +0530

    code changes

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ where:{
 })
 if(recordExist && recordExist?.name){
 console.log('exist:',recordExist);
-res.json({status:200,message:'record exist'})
+res.json({status:201,message:'record exist'})
 }else{
 console.log('not exist:');
 const job = await prisma.employee.create({
@@ -116,7 +116,7 @@ res.json({status:200,message:'created successfully'})
  app.get("/getdata", async (req, res) => {
   try {
     let getdetails;
-    if(req.query.daterange){
+     if(req.query.daterange){
       getdetails=await prisma.employee.findMany({
         where:{
           daterange:req.query.daterange
@@ -221,6 +221,74 @@ app.put("/timesheetActivity", async (req, res) => {
     console.error("Error creating user:", error);
   }
  
+});
+app.put('/modifyUserTimeSheet',async(req,res)=>{
+  try {
+    console.log('modify user req:',req.body);
+    const {timesheetsRows}=req.body;
+    timesheetsRows.map(async ele=>{
+      console.log('ele',ele);
+      await prisma.timesheet.update(
+        {
+        where:{id:Number(ele?.id)},
+        data:{
+              "projectCode": ele.projectCode,
+              "jobCode": ele.jobCode,
+              "day1": ele.day1,
+              "day2": ele.day2,
+              "day3": ele.day3,
+              "day4": ele.day4,
+              "day5": ele.day5,
+              "day6": ele.day6,
+              "day7": ele.day7,
+              "total": ele.total,
+              "status":''
+      }
+    })
+    })
+    res.json({success:true,message:'user timeSheet modified successfully.'});
+  } catch (error) {
+    console.log('error',error);
+    res.status(500).json({ success: false, message: "Error modify Employee TimeSheet Data" });
+  }
+});
+app.get("/getEmpTimeSheetdata", async (req, res) => {
+  try {
+    console.log('getdata:',req.query);
+    let getdetails;
+    if(req.query?.name && req.query.daterange){
+      getdetails=await prisma.employee.findMany({
+        where:{
+          name:req.query?.name,
+          daterange:req.query.daterange
+      },
+        include : {
+          timesheetsRows : true,
+          // where:{
+          //   projectCode:"WFS_1101"
+          // }
+        }
+      });
+    }
+    let mainArr=[];
+      for (let index = 0; index < getdetails.length; index++) {
+        const element = getdetails[index];
+        // console.log('ele',element);
+            element.timesheetsRows.map(ele=>{
+            ele.daterange=element.daterange             
+            ele.name=element.name
+            mainArr.push(ele);              
+          })
+      }
+      // console.log('mainArr',mainArr);
+      res.status(201).json(mainArr);
+    // res.status(201).json(getdetails);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Error listing employeedata" });
+    console.error("Error creating user:", error);
+  }
 });
 // app.put("/timesheetActivity", async (req, res) => {
 //   try{
